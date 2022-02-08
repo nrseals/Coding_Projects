c85636c5ea920cf195fa89dd13ee14a7c33be24d
Nrseals <nathan.r.seals@gmail.com>
2022-02-08 12:13:53 -0600

    login added

--- Python_Projects/flask.projects/dojo_tweets/server.py
+++ Python_Projects/flask.projects/dojo_tweets/server.py
@@ -39,18 +39,18 @@ def createUser():
     if not EMAIL_REGEX.match(request.form['email']):
         flash("Email must be vaild")
         valid = False
-    #Query db for duplicate email
+    # Query db for duplicate email
     db = connectToMySQL(SCHEMA)
     data = {
         'email': request.form['email']
     }
     query = "SELECT id FROM users WHERE email=%(email)s;"
     existing_users = db.query_db(query, data)
-    #Validate unique email
+    # Validate unique email
     if existing_users:
         flash("Email already in use")
         valid = False
-    #Pw - length, matches confirm pw
+    # Pw - length, matches confirm pw
     if len(request.form['pw']) < 8:
         flash("Password must be at least 8 characters")
         valid = False
@@ -61,14 +61,14 @@ def createUser():
         flash("Passwords must match")
         valid = False
 
-    #If not valid, redirect, display flash errors, don't add data to db
+    # If not valid, redirect, display flash errors, don't add data to db
     if not valid:
         return redirect('/')
-    
-    #hash user's Pw
+
+    # hash user's Pw
     pw_hash = bcrypt.generate_password_hash(request.form['pw'])
 
-    #create user in db, redirect to login
+    # create user in db, redirect to login
     db = connectToMySQL(SCHEMA)
     data = {
         'fn': request.form['fname'],
@@ -81,6 +81,57 @@ def createUser():
     flash("Registration successful! Please Log in!")
     return redirect('/')
 
+# Login - validate user input. if successfull, add info to session and redirect to homepage, if not, redirect to index with flash errors
+@app.route("/users/login", methods=['POST'])
+def login():
+    # Validate user input
+    valid = True
+    # Validate input length
+    if len(request.form['email']) < 1:
+        is_valid = False
+        flash("Please enter your email")
+    if len(request.form['password']) < 1:
+        is_valid = False
+        flash("Please enter your password")
+    #If not valid, return to index
+    if not valid:
+        return redirect("/")
+    #If length is valid, compare input to db and validate
+    else:
+        # query db to see if user email exists
+        db = connectToMySQL(SCHEMA)
+        data = {
+            'em': request.form['email']
+        }
+        query = "SELECT * FROM users WHERE email = %(em)s;"
+        #Result of Query is a List of dictonaries. Because email must be unique, there should only be one object that we store in the user variable
+        user = db.query_db(query, data)
+        # if user email exists and is unique then check pw
+        if user:
+            # Grab the stored hash from the db
+            # User info stored in list of objects, hence we refer to it as user[0]
+            hashed_pw = user[0]['password']
+            # hash and compare the user input password to the stored hash
+            if bcrypt.check_password_hash(hashed_pw, request.form['password']):
+                # if valid, add info to session, print message to console, redirect
+                session['user_id'] = user[0]['id']
+                user_id = session['user_id']
+                print("*"*50)
+                print(
+                    f'Login successful! User {user_id} added to session. Redirecting . . .')
+                return redirect("/tweets")
+            else: 
+                flash("Password is invalid")
+                return redirect("/")
+        # If email check fails
+        else:
+            flash("Please use a valid email address")
+            return redirect("/")
+
+#Homepage
+@app.route('/tweets')
+def tweets():
+    return render_template("tweets.html") 
 
 if __name__ == "__main__":
     app.run(debug=True)
