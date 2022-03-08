3c1eba838e6102bdc92f4c631b17781d16984cc9
Nrseals <nathan.r.seals@gmail.com>
2022-03-07 19:19:15 -0600

    3/7 7pm

--- dotnet_projects/ModuloCalculator/Modulo.cs
+++ dotnet_projects/ModuloCalculator/Modulo.cs
@@ -1,11 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //A simple application to calculate modulus 
-
 Console.WriteLine("Input num1");
-string num1S = Console.ReadLine();
+int num1 = System.Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input num2");
-string num2S = Console.ReadLine();
-int num1 = System.Convert.ToInt32(num1S);
-int num2 = System.Convert.ToInt32(num2S);
+int num2 = System.Convert.ToInt32(Console.ReadLine());
 int result = num1 % num2;
 Console.WriteLine($"Remainder is: {result}");
--- dotnet_projects/readme.md
+++ dotnet_projects/readme.md
@@ -1,7 +1,7 @@
 Welcome to my .NET Projects
 
-app
-> Created an applion that accepts two numbers and finds the remainder using Modulus 
+Mod caluclator
+> Created an application that accepts two numbers and finds the remainder using Modulus 
 
 Coffee
 >A simple console app that helps a user determine how to get the best tasting coffee possible based on how much coffee they want
